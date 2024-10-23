b9a64073097e9316708a6f595f92db2dc7bb37df
velbaumr <58743184+velbaumr@users.noreply.github.com>
2024-10-23 15:55:13 +0300

    connected api client, some fixes

--- src/components/RequestForm.tsx
+++ src/components/RequestForm.tsx
@@ -2,17 +2,30 @@ import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { calculateRequestedDays, calculateStartDate, calculateEndDate } from "../functions/FormCalculations";
+import vacationRequest from "../types/VacationRequest";
+import apiClient from "../services/ApiCilent";
 
 const RequestForm = () => {
     const navigate = useNavigate();
-    const [startDateValue, setStartDateValue] = useState<string | undefined>();
-    const [requestedDaysValue, setRequestedDaysValue] = useState<number | undefined>();
-    const [endDateValue, setEndDateValue] = useState<string | undefined>();
+    const [startDateValue, setStartDateValue] = useState<string>('');
+    const [requestedDaysValue, setRequestedDaysValue] = useState<number>(0);
+    const [endDateValue, setEndDateValue] = useState<string>('');
+    const [commentValue, setCommentValue] = useState<string | undefined>()
 
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        const result: vacationRequest = {
+            startDate: new Date(startDateValue),
+            endDate: new Date(endDateValue),
+            requestedDays: requestedDaysValue,
+            comment: commentValue || ''
+        };
+
+        apiClient.post<vacationRequest>('vacation_requests', result);
+        navigate('/');
     }
+
 
     const stringToDate = (dateString?: string): Date | undefined =>
         dateString ? new Date(dateString) : undefined;
@@ -22,10 +35,9 @@ const RequestForm = () => {
 
     return(
     <form onSubmit={onSubmit} className="col-md-4  offset-md-4">
-    <Form.Group>
+    <Form.Group  className="mb-3">
         <Form.Label  htmlFor="startDate">Start Date</Form.Label>
         <Form.Control type="date"
-                      className="form-control"
                       id="startDate"
                       value = {startDateValue || ''}
                       onChange={(e) => {
@@ -40,13 +52,15 @@ const RequestForm = () => {
                             setEndDateValue(dateToString(calculateEndDate(new Date(newStartDateValue), requestedDaysValue)));
                         }
                       }}/>
+        <Form.Control.Feedback type="invalid">
+
+        </Form.Control.Feedback>
     </Form.Group>
-    <Form.Group>
+    <Form.Group  className="mb-3">
         <Form.Label  htmlFor="requestedDays">Number of Requested Vacation Days</Form.Label>
         <Form.Control type="number"
-                      className="form-control"
                       id="requestedDays"
-                      value = {requestedDaysValue || 0}
+                      value = {requestedDaysValue}
                       onChange={(e) => {
                         const newRequestedDaysValue = Number(e.target.value);
                         setRequestedDaysValue(newRequestedDaysValue);
@@ -54,12 +68,17 @@ const RequestForm = () => {
                         if (startDateValue) {
                             setEndDateValue(dateToString(calculateEndDate(new Date(startDateValue), newRequestedDaysValue)));
                         }
+                        if (endDateValue && !startDateValue) {
+                            setStartDateValue(dateToString(calculateStartDate(new Date(endDateValue), newRequestedDaysValue)))
+                        }
                       }}/>
+    <Form.Control.Feedback type="invalid">
+
+    </Form.Control.Feedback>
     </Form.Group>
-    <Form.Group>
+    <Form.Group  className="mb-3">
         <Form.Label  htmlFor="endDate">End Date</Form.Label>
         <Form.Control type="date" 
-                      className="form-control"
                       id="endDate"
                       value = {endDateValue || ''}
                       onChange={(e) => {
@@ -74,10 +93,19 @@ const RequestForm = () => {
                             setStartDateValue(dateToString(calculateStartDate(new Date(newEndDateValue), requestedDaysValue)))
                         }
                       }} />
+    <Form.Control.Feedback type="invalid">
+
+    </Form.Control.Feedback>
     </Form.Group>
     <Form.Group className="mb-3">
         <Form.Label  htmlFor="comment">Comment</Form.Label>
-        <Form.Control as="textarea" rows={3} id="comment"/>
+        <Form.Control as="textarea"
+                      rows={3}
+                      id="comment"
+                      onChange={(e) => setCommentValue(e.target.value)}/>
+    <Form.Control.Feedback type="invalid">
+
+    </Form.Control.Feedback>
     </Form.Group>
     <Button type="submit"
         variant="primary"
--- src/components/RequestList.tsx
+++ src/components/RequestList.tsx
@@ -11,12 +11,12 @@ const RequestList = () => {
     }, []);
 
     return(
-    <table className="table table-hover">
+    <table className="table table-striped">
         <thead>
             <tr>
                 <th>Start Date</th>
                 <th>End Date</th>
-                <th>No of Requested Days</th>
+                <th>Requested vacation days</th>
                 <th>Comment</th>
             </tr>
         </thead>
@@ -24,8 +24,8 @@ const RequestList = () => {
             {
                 vacationRequests && vacationRequests.map((item: vacationRequest, index) => (
                     <tr key={index}>
-                        <td>{item.startDate.toString()}</td>
-                        <td>{item.endDate.toString()}</td>
+                        <td>{new Date(item.startDate).toLocaleDateString()}</td>
+                        <td>{new Date(item.endDate).toLocaleDateString()}</td>
                         <td>{item.requestedDays}</td>
                         <td>{item.comment}</td>
                     </tr>
